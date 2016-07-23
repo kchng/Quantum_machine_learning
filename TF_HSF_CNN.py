ffd0adbd324544a5e21c5a6c1955d77e23d6dbab
kchng <bihchyng@gmail.com>
2016-07-23 14:39:51 -0700

    3D CNN for HSF.

---
+++ TF_HSF_CNN.py
@@ -0,0 +1,176 @@
+import sys
+import numpy as np
+import data_reader
+
+
+filename = '/home/kelvin/Desktop/Theano test/HSF_N4x4x4_L200_U9_Mu0_UniformTGrid/N4x4x4_L200_U9_Mu0_T_shuffled_%.2d.HSF.stream'
+filename_weight_bias = "./wb.ckpt"
+filenumber = np.arange(1,41,1)
+HSF = data_reader.insert_file_info( filename, filenumber )
+HSF = HSF.categorize_data()
+
+n_feature_map1,n_feature_map2 = 64,128
+n_spin = 4
+n_time_dimension = 200
+n_output_neuron = 2
+n_fully_connected_neuron = 128
+
+filter_d = 2
+filter_h = filter_d
+filter_w = filter_d
+
+import tensorflow as tf
+# If you are not using an InteractiveSession, then you should build
+# the entire computation graph before starting a session and 
+# launching the graph.
+sess = tf.InteractiveSession()
+
+# x is a 2D-tensor and None means that a dimesion can be of any length,
+# but in this case, it corresponds to the batch size. To start building 
+# the computation graph, we'll create nodes for input images and target 
+# output classes. The target output classes y_ will consist of a 2D 
+# tensor, where each row is a one-hot (one-hot refers to a groups of 
+# bits among which only one is (1), the opposite is called one-cold) 2
+# -dimensional vector vector indicating which digit class the 
+# corresponding HSF data belongs to.
+x = tf.placeholder(tf.float32, [None, n_spin*n_spin*n_spin * n_time_dimension])
+y_ = tf.placeholder(tf.float32, [None, n_output_neuron])
+
+# To prevent 0 gradients and break symmetry, one should genereally
+# initialize weights with a small amount of noise for symmetry breaking.
+# To avoid "dead neurons" when using ReLU neurons, it is also a good
+# practice to initalize them with a slightly positive initial bias.
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+def bias_variable(shape):
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+
+def conv2d(x, W):
+    # The convolutions uses a stride of one and are zero padded so that
+    # the output is the same size as the input.
+    # tf.nn.conv2d(input, filter, strides, padding, 
+    #              use_cudnn_on_gpu=None, data_format=None, name=None)
+    return tf.nn.conv2d(x, W, strides=[1,1,1,1], padding='SAME')
+
+def conv3d(x, W):
+    # The convolutions uses a stride of one and are zero padded so that
+    # the output is the same size as the input.
+    # tf.nn.conv3d(input, filter, strides, padding, name=None)
+    return tf.nn.conv3d(x, W, strides = [1,1,1,1,1], padding='SAME')
+
+def max_pool_2x2(x):
+    # Max pooling over 2x2 blocks.
+    # tf.nn.max_pool(value, ksize, strides, padding, 
+    #                data_format='NHWC', name=None)
+    # value : shape [batch, height, width, channels]
+    # ksize : The size of the max pool window for each dimension of the
+    #         input tensor
+    # strides : The stride of the sliding window for each dimension of
+    #           the input tnesor.
+    return tf.nn.max_pool(x, ksize=[1,2,2,1], 
+                          strides=[1,2,2,1], padding='SAME')
+
+def max_pool_2x2x2(x):
+    # Max pooling over 2x2 blocks.
+    # tf.nn.max_pool(input, ksize, strides, padding, 
+    #                data_format='NHWC', name=None)
+    # input : shape [batch, depth, rows, cols, channels]
+    # ksize : The size of the max pool window for each dimension of the
+    #         input tensor
+    # strides : The stride of the sliding window for each dimension of
+    #           the input tnesor.
+    return tf.nn.max_pool3d(x, ksize=[1,2,2,2,1], 
+                          strides=[1,2,2,2,1], padding='SAME')
+
+# First Convolution Layer
+# The convolution will compute n features for each mxmxm block. Its weight
+# tensor will have a shape of [filter_Depth, filter_height, filter_width, 
+# in_channels, out_channels]].
+W_conv1 = weight_variable([filter_d,filter_h,filter_w,n_time_dimension,n_feature_map1])
+b_conv1 = bias_variable([n_feature_map1])
+
+# To apply the layer, first reshape x to a 4D tensor, with the second and
+# third dimensions correspondings to image width and height, and the final
+# dimension corresponding to the number of color channels.
+x_image = tf.reshape(x, [-1,n_spin,n_spin,n_spin,n_time_dimension])
+
+# Then convolve x_image with the weight tensor, add the bias, apply the
+# ReLU function, and finally max pool. After applying max pooling, the 
+# image has been reduced to 14x14.
+# n_feature_map1 x n_spin x n_spin x n_spin
+h_conv1 = tf.nn.relu(conv3d(x_image, W_conv1) + b_conv1)
+# n_feature_map1 x n_spin/2 x n_spin/2 x n_spini/2
+h_pool1 = max_pool_2x2x2(h_conv1)
+
+# Second Convolution Layer
+W_conv2 = weight_variable([filter_d,filter_h,filter_w,n_feature_map1,n_feature_map2])
+b_conv2 = bias_variable([n_feature_map2])
+
+# n_feature_map2 x n_spin/2 x n_spin/2 x n_spini/2
+h_conv2 = tf.nn.relu(conv3d(h_pool1, W_conv2) + b_conv2) 
+# n_feature_map2 x n_spin/4 x n_spin/4 x n_spini/4
+h_pool2 = max_pool_2x2x2(h_conv2)
+
+# Fully-connected Layer
+# Now add a fully-connected layer with n_fully_connected_neuron neurons to 
+# allow processing on the entire image. The tensor from the pooling layer 
+# is reshaped into a batch of vectors, multiply by a weight matrix, add a
+# bias, and apply a ReLU.
+W_fc1 = weight_variable([n_feature_map2*n_spin/4*n_spin/4*n_spin/4, n_fully_connected_neuron])
+b_fc1 = bias_variable([n_fully_connected_neuron])
+
+h_pool2_flat = tf.reshape(h_pool2, [-1, n_feature_map2*n_spin/4*n_spin/4*n_spin/4])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+
+# Dropout
+# To reduce overfitting, dropout will be applied before the readout layer.
+# We'll create a placeholder for the probability that a neuron's output is
+# kept during dropout. This allows us tro turn dropout on during training, and
+# turn it off during testing. TensorFlow's tf.nn.dropout op automatically 
+# handles scaling neuron outputs in addition to masking them, so droput just 
+# works without any additional scaling.
+keep_prob = tf.placeholder(tf.float32)
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+# Readout layer
+# Finally, a softmax regression layer is added.
+W_fc2 = weight_variable([n_fully_connected_neuron,n_output_neuron])
+b_fc2 = bias_variable([n_output_neuron]) 
+
+y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+
+# Train and Evaluate the Model
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv), reduction_indices=[1]))
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+# Before Variables can be used within a session, they must be initialized
+# using that session.
+sess.run(tf.initialize_all_variables())
+
+# Training the model can therefore be accomplished by repeatedly running 
+# train_step. Each training iteration load 50 training examples. Then, the
+# train_step operation can be run using feed_dict to replace the placeholder
+# tensors x and y_ with the training examples. Note: any tensor in the
+# computation graph can be replcaed using feed_dict.
+for i in range(3200):
+    batch = HSF.train.next_batch(ndata=32000)
+    if i%100 == 0 :
+        train_accuracy = accuracy.eval(feed_dict={
+                         x: batch[0], y_: batch[1], keep_prob: 1.0})
+        test_accuracy = accuracy.eval(feed_dict={
+                         x: HSF.test.images, y_: HSF.test.labels, keep_prob: 1.0})
+        print("step %d, training accuracy %g, test accuracy %g"%(i, train_accuracy, test_accuracy))
+    train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+
+print("test accuracy %g"%accuracy.eval(feed_dict={
+    x: HSF.test.images, y_: HSF.test.labels, keep_prob: 1.0}))
+
+save_weights_and_biases = tf.train.Saver([W_conv1, b_conv1, W_conv2, b_conv2, W_fc1, b_fc1, W_fc2, b_fc2])
+saver.save(sess,filename_weight_bias)
+
+
+
