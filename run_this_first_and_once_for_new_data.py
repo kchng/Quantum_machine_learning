aff5e9c60cd4845eafaee8bf758e03e5edaa0bd0
kchng <bihchyng@gmail.com>
2016-07-30 19:03:23 -0700

    Updated the file so that it convert file names to consistent file number formating and added the ability to change the system (Hubbard model) information in one place)

--- run_this_first_and_once_for_new_data.py
+++ run_this_first_and_once_for_new_data.py
@@ -1,36 +1,39 @@
-import randomize_file_data
+import randomize_file_data_tmp
 import numpy as np
+import os
 import time
 import sys
-import os
 
 # If you are in the directory where the data file are located, make sure to modify the number formatting accordingly. Here it is %.3f. If not, give the full file path and modify the number formatting accordingly. Make sure the data file have consisten number formatting, i.e. 3 decimal places.
 
-filename = './N4x4x4_L200_U9_Mu0_T%.3f.HSF.stream'
+# Potential energy
+U = 9
+# System size
+nspin = 4
+# Critical temperature
+Tc = 0.36
+# Memory setting
+memory_setting = 'medium'
 
-# Then provide the full list of file number (WITHOUT including the one at T_c)
+filename = './N%dx%dx%d_L200_U%d_Mu0_T' % (nspin,nspin,nspin,U) + '%.3f.HSF.stream'
 
-os.system("ls -l N4x4x4_L200_U9_Mu0_T* | awk '{print $9}' | sed -e s/N4x4x4_L200_U9_Mu0_T//g -e s/.HSF.stream//g > dtau.dat")
+# Get temperature
+os.system("ls -l N%dx%dx%d_L200_U%d_Mu0_T*.HSF.stream | awk '{print $9}' | sed -e s/N%dx%dx%d_L200_U%d_Mu0_T//g -e s/.HSF.stream//g > dtau.dat" %(nspin,nspin,nspin,U,nspin,nspin,nspin,U))
+
+# Load temperature into a list of string
 dtau = np.genfromtxt("dtau.dat",dtype='str')
+
+# Rename files for consistent formating
 for i in range(len(dtau)):
-   os.system("mv N4x4x4_L200_U9_Mu0_T%s.HSF.stream N4x4x4_L200_U9_Mu0_T%.3f.HSF.stream" %(dtau[i],float(dtau[i])))
-os.system("ls -l N4x4x4_L200_U9_Mu0_T* | awk '{print $9}' | sed -e s/N4x4x4_L200_U9_Mu0_T//g -e s/.HSF.stream//g > dtau.dat")
+   os.system("mv N%dx%dx%d_L200_U%d_Mu0_T%s.HSF.stream N%dx%dx%d_L200_U%d_Mu0_T%.3f.HSF.stream" % (nspin,nspin,nspin,U,dtau[i],nspin,nspin,nspin,U,float(dtau[i])) )
+
+# Get temperature (with consistent formating) 
+os.system("ls -l N%dx%dx%d_L200_U%d_Mu0_T*.HSF.stream | awk '{print $9}' | sed -e s/N%dx%dx%d_L200_U%d_Mu0_T//g -e s/.HSF.stream//g > dtau.dat" % (nspin,nspin,nspin,U,nspin,nspin,nspin,U) )
+
 dtau = np.genfromtxt("dtau.dat")
 
-Tc = 0.36
-
-dtau = dtau[dtau!=Tc]
-
-#dtau = np.array([0.060, 0.075, 0.090, 0.105, 0.120, 0.135, 0.150, 0.165, \
-#                 0.180, 0.195, 0.210, 0.225, 0.240, 0.255, 0.270, 0.285, \
-#                 0.300, 0.315, 0.330, 0.345, 0.510, 0.660, 0.810, \
-#                 0.960, 1.110, 1.260, 1.410, 1.560, 1.710, 1.860, 2.010, \
-#                 2.160, 2.310, 2.460, 2.610, 2.760, 2.910, 3.060, 3.210, \
-#                 3.360])
-
 # Initilize the python module by giving it the file information
-initialize = randomize_file_data.insert_file_info( filename, dtau, boundary = Tc )
+initialize = randomize_file_data_tmp.insert_file_info( filename, dtau, boundary = Tc )
 
 # Start randomizing data. Sit back and have a cup of coffee, it needs a bit of time. On an i5 3.1 GHz, it takes about 24 minutes on the medium setting. If your computer have more than 8 GB of memory, set it to high and you should expect a 30% reduction in time on a comparable processor.
-initialize.randomize_data(memory_size = 'high',shuffle_data=True)
-
+initialize.randomize_data(memory_size = memory_setting)
