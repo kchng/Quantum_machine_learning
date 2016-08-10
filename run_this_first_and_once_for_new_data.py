76f38ea9c46b7dee36ed11619680368d17aeeab0
kchng <bihchyng@gmail.com>
2016-08-09 23:24:49 -0700

    Added the ability to process data set from two different Us and label the temperature index of each file uniquely by offsetting the temperature index of the second data set by len(dtau).

--- run_this_first_and_once_for_new_data.py
+++ run_this_first_and_once_for_new_data.py
@@ -6,36 +6,79 @@ import sys
 
 # If you are in the directory where the data file are located, make sure to modify the number formatting accordingly. Here it is %.3f. If not, give the full file path and modify the number formatting accordingly. Make sure the data file have consisten number formatting, i.e. 3 decimal places.
 
-# Potential energy
-U = 9
+use_single_U = T
+if use_single_U :
+    # Potential energy
+    U1 = 9
+    # Critical temperature
+    Tc1 = 0.36
+else :
+    # Potential energy 1 
+    U1 = 4
+    # Critical temperature
+    Tc1 = 0.16
+    # Potential energy 2
+    U2 = 20
+    # Critical temperature
+    Tc1 = 0.19
+    print 'Processing data set from two Us.'
+
+
 # System size
 n_x = 4
 # Imaginary time
 L = 200
-# Critical temperature
-Tc = 0.36
 # Memory setting
 memory_setting = 'medium'
 
-filename = './N%dx%dx%d_L200_U%d_Mu0_T' % (n_x,n_x,n_x,U) + '%.3f.HSF.stream'
+filename = './N%dx%dx%d_L%d_U%d_Mu0_T' % (n_x,n_x,n_x,L,U1) + '%.3f.HSF.stream'
 
 # Get temperature and save them to a file.
-os.system("ls -l N%dx%dx%d_L%d_U%d_Mu0_T*.HSF.stream | awk '{print $9}' | sed -e s/N%dx%dx%d_L%d_U%d_Mu0_T//g -e s/.HSF.stream//g > dtau.dat" %(n_x,n_x,n_x,L,U,n_x,n_x,n_x,L,U))
+os.system("ls -l N%dx%dx%d_L%d_U%d_Mu0_T*.HSF.stream | awk '{print $9}' | sed -e s/N%dx%dx%d_L%d_U%d_Mu0_T//g -e s/.HSF.stream//g > dtau.dat" %(n_x,n_x,n_x,L,U1,n_x,n_x,n_x,L,U1))
 
 # Load temperature into a list of string
 dtau = np.genfromtxt("dtau.dat",dtype='str')
 
 # Rename files for consistent formating
 for i in range(len(dtau)):
-   os.system("mv N%dx%dx%d_L%d_U%d_Mu0_T%s.HSF.stream N%dx%dx%d_L%d_U%d_Mu0_T%.3f.HSF.stream" % (n_x,n_x,n_x,L,U,dtau[i],n_x,n_x,n_x,L,U,float(dtau[i])) )
+    os.system("mv N%dx%dx%d_L%d_U%d_Mu0_T%s.HSF.stream N%dx%dx%d_L%d_U%d_Mu0_T%.3f.HSF.stream" % (n_x,n_x,n_x,L,U1,dtau[i],n_x,n_x,n_x,L,U1,float(dtau[i])) )
 
 # Get reformatted temperature and save them to a file.
-os.system("ls -l N%dx%dx%d_L%d_U%d_Mu0_T*.HSF.stream | awk '{print $9}' | sed -e s/N%dx%dx%d_L%d_U%d_Mu0_T//g -e s/.HSF.stream//g > dtau.dat" % (n_x,n_x,n_x,L,U,n_x,n_x,n_x,L,U) )
+os.system("ls -l N%dx%dx%d_L%d_U%d_Mu0_T*.HSF.stream | awk '{print $9}' | sed -e s/N%dx%dx%d_L%d_U%d_Mu0_T//g -e s/.HSF.stream//g > dtau.dat" % (n_x,n_x,n_x,L,U1,n_x,n_x,n_x,L,U1))
 
 dtau = np.genfromtxt("dtau.dat")
 
 # Initilize the python module by giving it the file information
-initialize = randomize_file_data.insert_file_info( filename, dtau, boundary = Tc )
+initialize = randomize_file_data.insert_file_info( filename, dtau, boundary = Tc1 )
 
 # Start randomizing data. Sit back and have a cup of coffee, it needs a bit of time. On an i5 3.1 GHz, it takes about 24 minutes on the medium setting. If your computer have more than 8 GB of memory, set it to high and you should expect a 30% reduction in time on a comparable processor.
 initialize.randomize_data(memory_size = memory_setting)
+
+if not(use_single_U) :
+    offset = len(dtau)
+  
+    filename = './N%dx%dx%d_L%d_U%d_Mu0_T' % (n_x,n_x,n_x,L,U2) + '%.3f.HSF.stream'
+
+    # Get temperature and save them to a file.
+    os.system("ls -l N%dx%dx%d_L%d_U%d_Mu0_T*.HSF.stream | awk '{print $9}' | sed -e s/N%dx%dx%d_L%d_U%d_Mu0_T//g -e s/.HSF.stream//g > dtau.dat" %(n_x,n_x,n_x,L,U2,n_x,n_x,n_x,L,U2))
+
+    # Load temperature into a list of string
+    dtau = np.genfromtxt("dtau.dat",dtype='str')
+
+    # Rename files for consistent formating
+    for i in range(len(dtau)):
+        os.system("mv N%dx%dx%d_L%d_U%d_Mu0_T%s.HSF.stream N%dx%dx%d_L%d_U%d_Mu0_T%.3f.HSF.stream" % (n_x,n_x,n_x,L,U2,dtau[i],n_x,n_x,n_x,L,U2,float(dtau[i])) )
+
+    # Get reformatted temperature and save them to a file.
+    os.system("ls -l N%dx%dx%d_L%d_U%d_Mu0_T*.HSF.stream | awk '{print $9}' | sed -e s/N%dx%dx%d_L%d_U%d_Mu0_T//g -e s/.HSF.stream//g > dtau.dat" % (n_x,n_x,n_x,L,U2,n_x,n_x,n_x,L,U2))
+
+    dtau = np.genfromtxt("dtau.dat")
+
+    # Initilize the python module by giving it the file information
+    initialize = randomize_file_data.insert_file_info( filename, dtau, boundary = Tc1, temp_index_offset=offset )
+
+    # Start randomizing data. Sit back and have a cup of coffee, it needs a bit of time. On an i5 3.1 GHz, it takes about 24 minutes on the medium setting. If your computer have more than 8 GB of memory, set it to high and you should expect a 30% reduction in time on a comparable processor.
+    initialize.randomize_data(memory_size = memory_setting)
+
+os.remove("dtau.dat")
+print 'Done.'    
